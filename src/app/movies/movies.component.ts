9e0ae7dfeabe958d8c4672d2147fd4bcf1ca0b33
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-15 17:13:01 +0300

    1-Component-Adding

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+
+@Component({
+    selector: 'movies', // ==> <movies></movies> , '.movies'==> <div class = "movies">
+    templateUrl: 'movies.component.html'
+})
+export class MoviesComponent {
+
+}
