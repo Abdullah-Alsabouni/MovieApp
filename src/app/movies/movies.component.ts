c94f21cf0c94e708e1d18a8604da0261f2abd09a
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-16 11:46:07 +0300

    Lesson 3-Component style

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,11 +1,16 @@
 import { Component } from "@angular/core";
 
-
-
-
 @Component({
     selector: 'movies', // ==> <movies></movies> , '.movies'==> <div class = "movies">
-    templateUrl: 'movies.component.html'
+    templateUrl: 'movies.component.html',
+    styles: [`
+        h2 {
+            color: blue;
+        }
+        // h4 {
+        //     color: yellow;  // movies comp. içinde olan movie comp. h4 ile tanımlanan etiketler movies.component.style'den ayarlanamaz
+        // }
+        `]
 })
 export class MoviesComponent {
 
