0c3dc44b40f9bed5aad655cee2aec047de5acad6
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-18 15:35:05 +0300

    Lesson 6-Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MovieComponent } from './movie/movie.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Movies } from '../movie.datasource';
+import { Movie } from '../movie';
 
 
 @Component({
-  selector: 'movies', // ==> <movies></movies> , '.movies'==> <div class = "movies">
+  selector: 'movies',
   templateUrl: 'movies.component.html'
 })
 export class MoviesComponent {
   title = 'Movie List';
-  movies = Movies; // movies movie.datasource.ts'ten gelen Movie dizisine eşit
+  movies = Movies;
+  selectedMovie: Movie | null = null;
+
+  
+  onSelect(movie: Movie): void { // Listede seçilen bir elemanı selectedMovie'ye atama işlemi
+      this.selectedMovie = movie;  
+  }
 }
