c4e1b5624f61f8468046c1e01fffaf1c652e1912
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-18 16:07:02 +0300

    Lesson 7-Input-Properties

---
+++ src/app/movie-detail/movie-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../movie';
+
+
+@Component({
+  selector: 'movie-detail',
+  templateUrl: './movie-detail.component.html',
+  styleUrl: './movie-detail.component.css'
+})
+export class MovieDetailComponent {
+
+  @Input() movie: Movie | null = null  // @Input() dışarıdan gelen Input'lar için kullanılır
+}
