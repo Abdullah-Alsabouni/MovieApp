339ad1b3f175beb175e13fc3983be411928406cb
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-19 13:42:05 +0300

    Lesson 9-Observable

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,28 +2,30 @@ import { Component } from '@angular/core';
 import { Movie } from '../movie';
 import { MovieService } from '../movie.service';
 
-
 @Component({
   selector: 'movies',
-  templateUrl: 'movies.component.html'
+  templateUrl: 'movies.component.html',
 })
 export class MoviesComponent {
   title = 'Movie List';
-  movies: Movie[] | null = null; 
+  movies: Movie[] | null = null;
   selectedMovie: Movie | null = null;
 
-  constructor(private movieService: MovieService){} // MovieSerive'ten bir nesne Injectable et
+  constructor(private movieService: MovieService) {} // MovieSerive'ten bir nesne Injectable et
 
-  
-  onSelect(movie: Movie): void { // Listede seçilen bir elemanı selectedMovie'ye atama işlemi
-      this.selectedMovie = movie;  
+  onSelect(movie: Movie): void {
+    // Listede seçilen bir elemanı selectedMovie'ye atama işlemi
+    this.selectedMovie = movie;
   }
 
   getMovies(): void {
-    this.movies = this.movieService.getMovies();
-  } 
+    this.movieService.getMovies().subscribe((movies) => { // subscribe: Obsevable'den gelen bilgiyi yakalar
+      this.movies = movies;
+    });
+  }
 
-  ngOnInit(): void { // nesne üretildikten sonra çalışır
+  ngOnInit(): void {
+    // nesne üretildikten sonra çalışır
     this.getMovies();
   }
 }
